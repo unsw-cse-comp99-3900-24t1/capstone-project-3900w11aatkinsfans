eab366a9d7f6b05daea766d754edbfd4e681201b
Atkins Fan <z5260088@ad.unsw.edu.au>
2024-06-23 16:56:33 +1000

    fixed frontend backend connection, basic setup done

--- frontend/src/pages/Test.jsx
+++ frontend/src/pages/Test.jsx
@@ -1,38 +1,22 @@
-// import React from 'react';
-
-// export default function Test () {
-
-
-//   return (
-//     <>
-//       <div>
-//         Test: {message}
-//       </div>
-//     </>
-//   )
-// }
-
 import React from 'react';
 
 export default function Test() {
-
-  // const [message, setMessage] = useState('');
-  // setMessage('deine mutter');
-  // React.useEffect(() => {
-  //   fetch(process.env.REACT_APP_BACKEND_URL + '/test' || 'http://localhost:5000/test')
-  //     .then(response => {
-  //       if (!response.ok) {
-  //         throw new Error('Bad response');
-  //       }
-  //       return response.json();
-  //     })
-  //     .then(data => {
-  //       setMessage(data.message);
-  //     })
-  //     .catch(err => {
-  //       setMessage('FAILED, as something related to fetching backend data is not working :(');
-  //     });
-  // }, []);
+  const [message, setMessage] = React.useState('yes');
+  React.useEffect(() => {
+    fetch(process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000/test')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Bad response');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setMessage(data.message);
+      })
+      .catch(err => {
+        setMessage('FAILED, as something related to fetching backend data is not working :(');
+      });
+  }, []);
 
   return (
     <div>
@@ -41,7 +25,7 @@ export default function Test() {
       <br></br>
       <br></br>
       <p>This is the Test component content.</p>
-      {/* <p>Test: {message} </p> */}
+      <p>Test: {message} </p>
     </div>
   );
 };
